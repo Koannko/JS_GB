fe89c28e63b0b441cf9fddc324f40fdb3e21fa54
Koannko <kostyleva-2000@bk.ru>
2022-06-04 15:42:27 +0500

    init: lesson 2

---
+++ script.js
@@ -0,0 +1,39 @@
+const goods = [
+  { title: 'Shirt', price: 150 },
+  { title: 'Socks', price: 50 },
+  { title: 'Jacket', price: 350 },
+  { title: 'Shoes', price: 250 },
+];
+
+class GoodsItem {
+  constructor({ title, price }) {
+    this.title = title;
+    this.price = price;
+  }
+  render() {
+    return `
+    <div class="goods-item">
+      <h3>${this.title}</h3>
+      <p>${this.price}</p>
+    </div>
+  `;
+  }
+}
+class GoodsList {
+  items = [];
+  fetchGoods() {
+    this.items = goods;
+  }
+  render() {
+    const goods = this.items.map(item => {
+      const goodItem = new GoodsItem(item);
+      return goodItem.render()
+    }).join('');
+  
+    document.querySelector('.goods-list').innerHTML = goods;
+  }
+}
+
+const goodsList = new GoodsList();
+goodsList.fetchGoods();
+goodsList.render();
